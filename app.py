cbb3ffc0cea90efe46f38d7c3f0ae134ec00c43d
khushi-0-1 <143900327+khushi-0-1@users.noreply.github.com>
2025-04-17 23:38:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -53,4 +53,7 @@ def predict():
         return f"Error: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render or Railway will inject PORT
+    app.run(host='0.0.0.0', port=port)
+
